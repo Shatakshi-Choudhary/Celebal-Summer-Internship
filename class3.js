fffa189786a666c4d591004f987d21061ec6e1e4
Shatakshi Choudhary <87706725+Shatakshi-Choudhary@users.noreply.github.com>
2023-07-01 12:48:24 +0530

    Add files via upload

---
+++ class3.js
@@ -0,0 +1,17 @@
+// Function in javascript
+
+function name(para1, para2) {
+
+}
+
+function multiplication(x, y, c = 0) {
+    return x * y * c
+}
+let z = multiplication(5, 10, 7)
+console.log(z);
+
+// Arrow functions
+mul = (a, b) => {
+    return a * b;
+}
+console.log(mul); 
